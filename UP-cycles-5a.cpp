f1a7287891f6a4962bf91c3fd001bb095e9450db
Dilyana Gineva <dilyana_vp@abv.bg>
2015-11-05 21:36:11 +0200

    Упр. 2, зад. 5а

---
+++ UP-cycles-5a.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+using namespace std;
+
+int main ()
+{
+  int a, sum=0;
+
+    do
+    {
+        cout << "Enter number: ";
+        cin >> a;
+        cout << "Your number is: " << a << endl;
+
+        if (a <= 30)
+          {
+            sum += a;
+            cout << sum << endl;
+          }
+    }
+    while (sum <= 100);
+
+return 0;
+}
